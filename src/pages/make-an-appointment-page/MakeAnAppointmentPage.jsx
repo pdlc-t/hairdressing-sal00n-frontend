0db8ab0a5a2d8df16a2ac5553698ae8b92b04092
pdlc-t <ptdobrosielski@gmail.com>
2025-03-16 12:58:32 +0100

    Implemented services list (in outline, no backend integration yet). Further visual improvements to the MakeAnAppointmentPage

--- src/pages/make-an-appointment-page/MakeAnAppointmentPage.jsx
+++ src/pages/make-an-appointment-page/MakeAnAppointmentPage.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import classes from './make-an-appointment-page.module.css'
 import AppointmentCalendar from '../../components/appointment-calendar/AppointmentCalendar'
+import ServicesList from '../../components/services-list/ServicesList'
 
 const MakeAnAppointmentPage = () => {
   return (
     <div className={`${classes.main}`}>
       <div className={`${classes.serviceSelectContainer}`}>
-        
+        <ServicesList />
       </div>
       <div className={`${classes.calendarContainer}`}>
         <AppointmentCalendar />
