3c72b6b387f851d80fffa70fae76bb86ddd6bb08
pdlc-t <ptdobrosielski@gmail.com>
2025-03-17 00:32:23 +0100

    Services are now read from a table collection of service names. Added a functionality to choose a service and higlight this choice, only one service can be chosen and highlighted at a given time.

--- src/components/services-list/ServiceCard.jsx
+++ src/components/services-list/ServiceCard.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import classes from './services-list.module.css'
 import scissors from '../../assets/svg_images/service_icons/scissors.svg'
 
-const ServiceCard = ({ serviceName }) => {
+const ServiceCard = ({ serviceName, highlightedService, setHighlighted }) => {
   return (
-    <div className={`${classes.serviceCardContainer}`}>
+    <div className={`${classes.serviceCardContainer} 
+      ${highlightedService === serviceName ? classes.highlighted : ''}`}
+      onClick={() => setHighlighted(prev => serviceName === prev ? null : serviceName)}>
       <p className={`${classes.serviceName}`}>{serviceName}</p>
       <img src={scissors} alt="service icon" className={`${classes.serviceIcon}`}/>
     </div>
--- src/components/services-list/ServicesList.jsx
+++ src/components/services-list/ServicesList.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ServiceCard from './ServiceCard'
 import classes from './services-list.module.css'
 
 const ServicesList = () => {
+  const [highlightedService, setHighlightedService] = useState();
+
   return (
     <div className={`${classes.mainContainer}`}>
       <h1>Choose Service</h1>
-      <ServiceCard serviceName={"Strzyżenie 1"}/>
-      <ServiceCard serviceName={"Strzyżenie 2"}/>
-      <ServiceCard serviceName={"Strzyżenie 3"}/>
-      <ServiceCard serviceName={"Strzyżenie 4"}/>
-      <ServiceCard serviceName={"Strzyżenie 5"}/>
+
+      {/* TODO: enable handling whole service, not just the name */}
+      {["Strzyżenie 1", "Strzyżenie 2", "Strzyżenie 3", "Strzyżenie 4", "Strzyżenie 5"].map((service, index) => (
+        <ServiceCard
+          key={index}
+          serviceName={service}
+          highlightedService={highlightedService}
+          setHighlighted={setHighlightedService}
+        />
+      ))}
     </div>
   )
 }
