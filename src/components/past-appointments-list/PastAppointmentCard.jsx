cdde71fd9b581a8b174707bc6cc650b39ac1aac7
alis1 <example.com>
2025-05-03 20:59:33 +0200

    Rating

--- src/components/past-appointments-list/PastAppointmentCard.jsx
+++ src/components/past-appointments-list/PastAppointmentCard.jsx
@@ -1,16 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from './past-appointmenst-list.module.css'
 import scissors from '../../assets/svg_images/service_icons/scissors.svg'
 
-const PastAppointmentCard = ({service, date}) => {
-  return (
-    <div className={`${classes.cardContainer}`}>
-      <header className={`${classes.serviceName}`}>{service}</header>
-      <header className={`${classes.date}`}>{date}</header>
-      <div className={`${classes.iconContainer}`}>
-        <img src={scissors} alt="scissors icon" className={`${classes.icon}`} />
+const PastAppointmentCard = ({ service, date, rating }) => {
+  const [currentRating, setCurrentRating] = useState(rating)
+
+  const handleRatingClick = (newRating) => {
+    setCurrentRating(newRating)
+    // Możesz dodać tutaj wywołanie zapisu do backendu, np.:
+    // saveRating(serviceId, newRating)
+  }
+
+  const renderStars = () => {
+    const totalStars = 5
+    return (
+      <div className={classes.stars}>
+        {[...Array(totalStars)].map((_, i) => (
+          <span
+            key={i}
+            className={i < currentRating ? classes.filledStar : classes.emptyStar}
+            onClick={() => handleRatingClick(i + 1)}
+            style={{ cursor: 'pointer' }}
+          >
+            ★
+          </span>
+        ))}
       </div>
-      <section className={`${classes.detailsButton}`}>details</section>
+    )
+  }
+
+  return (
+    <div className={classes.cardContainer}>
+      <header className={classes.serviceName}>{service}</header>
+      <header className={classes.date}>{date}</header>
+      <div className={classes.iconContainer}>
+        <img src={scissors} alt="scissors icon" className={classes.icon} />
+      </div>
+      {renderStars()}
+      <section className={classes.detailsButton}>details</section>
     </div>
   )
 }
--- src/components/past-appointments-list/PastAppointmentsList.jsx
+++ src/components/past-appointments-list/PastAppointmentsList.jsx
@@ -1,17 +1,22 @@
+// src/components/past-appointments-list/PastAppointmentsList.jsx
+
 import React from 'react'
 import PastAppointmentCard from './PastAppointmentCard'
 import classes from './past-appointmenst-list.module.css'
 import appointments from '../../test_data/mock_services.json'
 
 const PastAppointmentsList = () => {
-
   return (
-    <div className={`${classes.appointmentsList}`}>
+    <div className={classes.appointmentsList}>
       <h1>Past Appointments</h1>
       <ul>
         {appointments.map((service, index) => (
           <li key={index}>
-            <PastAppointmentCard service={service.serviceName} date={service.date} />
+            <PastAppointmentCard
+              service={service.serviceName}
+              date={service.date}
+              rating={service.rating}
+            />
           </li>
         ))}
       </ul>
