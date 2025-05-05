b9f9722e4e81fd335923ecb6479c234f9c3e508b
Fufuk <ptdobrosielski@gmail.com>
2025-05-05 18:54:20 +0200

    Added a functionality to fetch appointments from the backend and color the day tiles in appointment calendar accordingly

--- src/components/appointment-calendar/CalendarDayTile.jsx
+++ src/components/appointment-calendar/CalendarDayTile.jsx
@@ -2,17 +2,20 @@ import React, { useContext } from 'react'
 import classes from './appointment-calendar.module.css'
 import { MakingAppointmentContext } from '../../pages/make-an-appointment-page/MakeAnAppointmentPage';
 
-const CalendarDayTile = ({ day, isFromActiveMonth, isToday }) => {
+const CalendarDayTile = ({ day, isFromActiveMonth, isToday, availability }) => {
   
   const { toggleAppointmentPopup } = useContext(MakingAppointmentContext);
+
+  const availabilityClass = classes[availability] || '';
 
   return (
     <div className={`
       ${classes.dayTile}
       ${isFromActiveMonth ? '' : classes.blackedout}
       ${isToday ? classes.currentDay : ''}
+      ${availabilityClass}
     `}
-    onClick={isFromActiveMonth ? toggleAppointmentPopup : undefined}>
+    onClick={isFromActiveMonth && availability !== "red" ? toggleAppointmentPopup : undefined}>
       {day}
     </div>
   )
--- src/components/appointment-calendar/DaysGrid.jsx
+++ src/components/appointment-calendar/DaysGrid.jsx
@@ -1,24 +1,67 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CalendarDayTile from './CalendarDayTile'
 import classes from './appointment-calendar.module.css'
 import { DateTime, Interval } from 'luxon'
+//import appointmentsData from '../../test_data/example_appointments.json';
+
+// TODO: unplug test json file with appointments and fetch data from backend (uncomment necessary lines)
 
 const DaysGrid = ({ firstDayOfActiveMonth }) => {
+  const [appointmentsData, setAppointmentsData] = useState([]);
   const today = DateTime.local();
   // const firstDayOfActiveMonth = today.startOf("month");
   const daysOfMonth = Interval.fromDateTimes(
     firstDayOfActiveMonth.startOf("week"),
     firstDayOfActiveMonth.endOf("month").endOf("week")
-  ).splitBy({day: 1}).map(day => day.start);
+  ).splitBy({ day: 1 }).map(day => day.start);
+
+  useEffect(() => {
+    const fetchAppointments = async () => {
+      const response = await fetch('http://127.0.0.1:5000/appointments/get-appointments');
+      const data = await response.json();
+
+      setAppointmentsData(data);
+    }
+
+    fetchAppointments();
+  }, [])
+
+  useEffect(() => {
+    console.log(appointmentsData);
+  }, [appointmentsData])
+
+  const determineAvailability = (day) => {
+    const matchingAppointments = appointmentsData.filter((appointment) => {
+      const appointmentDate = DateTime.fromISO(appointment.date).startOf('day');
+      console.log(appointmentDate);
+      const targetDay = day.startOf('day');
+
+      return appointmentDate.hasSame(targetDay, 'day');
+    })
+    console.log(matchingAppointments.length)
+    if (matchingAppointments.length < 2) {
+      console.log("green")
+      return "green";
+    } else if (matchingAppointments.length <= 4) {
+      console.log("orange")
+      return "orange";
+    } else {
+      console.log("red")
+      return "red"
+    }
+  }
+
+  if (!appointmentsData.length) return <h1>Fetching days...</h1>
 
   return (
     <div className={`${classes.daysGrid}`}>
       {daysOfMonth.map((day, dayIndex) => (
-        <CalendarDayTile 
-          key={dayIndex} 
+        <CalendarDayTile
+          key={dayIndex}
           day={day.day}
           isFromActiveMonth={day.month === firstDayOfActiveMonth.month}
-          isToday={day.day === today.day && day.month === today.month && day.year === today.year} 
+          isToday={day.day === today.day && day.month === today.month && day.year === today.year}
+          availability={determineAvailability(day)}
         />
       ))}
     </div>
